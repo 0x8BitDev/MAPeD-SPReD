13f9edb2fce46b23786ce427859b9d63628ced08
0x8BitDev <49449254+0x8BitDev@users.noreply.github.com>
2023-05-01 21:38:18 +0300

    MAPeD: data export: all the data export related files have been moved to the new folder - 'export'; +added 'compressors.cs' and 'exporter_main.cs';

---
+++ src/MAPeD/src/export/compressors.cs
@@ -0,0 +1,182 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: 0x8BitDev Copyright 2023 ( MIT license. See LICENSE.txt )
+ * Date: 01.05.2023
+ * Time: 19:24
+ */
+using System;
+
+namespace MAPeD
+{
+	/// <summary>
+	/// Description of compressors.
+	/// </summary>
+	public interface i_compressor
+	{
+		int	encode8( byte[] _src_arr, ref byte[] _encoded_arr );
+		int	encode16( ushort[] _src_arr, ref ushort[] _encoded_arr );
+	}
+	
+	public class compressor_RLE : i_compressor
+	{
+		public compressor_RLE()
+		{
+			//...
+		}
+		
+		// RLE routine from NESst tool by Shiru
+		public int encode8( byte[] _arr, ref byte[] _rle_arr )
+		{
+			_rle_arr = new byte[ _arr.Length ];
+			
+			int[] stat = new int[ byte.MaxValue + 1 ];
+			int i,tag,sym,sym_prev,len,ptr;
+			
+			int size = _arr.Length;
+			
+			Array.Clear( stat, 0, stat.Length );
+			
+			for( i = 0; i < size; ++i ) ++stat[ _arr[ i ] ];
+		
+			tag = -1;
+		
+			for( i = 0; i < stat.Length; ++i )
+			{
+				if( stat[ i ] == 0 )
+				{
+					tag = i;
+					break;
+				}
+			}
+			
+			if( tag < 0 )
+			{
+				return -1;
+			}
+		
+			ptr = 0;
+			len = 1;
+			sym_prev = -1;
+		
+			_rle_arr[ ptr++ ] = ( byte )tag;
+			
+			for( i = 0; i < size; ++i )
+			{
+				sym = _arr[ i ];
+		
+				if( sym_prev != sym || len >= byte.MaxValue || i == size - 1 )
+				{
+					if( ptr >= _arr.Length )
+					{
+						return -1;
+					}
+					
+					if( len > 1 )
+					{
+						if( len == 2 )
+						{
+							_rle_arr[ ptr++ ] = ( byte )sym_prev;
+						}
+						else
+						{
+							_rle_arr[ ptr++ ] = ( byte )tag;
+							_rle_arr[ ptr++ ] = ( byte )( len - 1 );
+						}
+					}
+		
+					_rle_arr[ ptr++ ] = ( byte )sym;
+		
+					sym_prev = sym;
+		
+					len = 1;
+				}
+				else
+				{
+					++len;
+				}
+			}
+		
+			_rle_arr[ ptr++ ] = ( byte )tag;	//end of file marked with zero length rle
+			_rle_arr[ ptr++ ] = 0;
+			
+			return ptr;
+		}
+
+		public int encode16( ushort[] _arr, ref ushort[] _rle_arr )
+		{
+			_rle_arr = new ushort[ _arr.Length ];
+			
+			int[] stat = new int[ ushort.MaxValue + 1 ];
+			int i, tag, sym, sym_prev, len, ptr;
+			
+			int size = _arr.Length;
+			
+			Array.Clear( stat, 0, stat.Length );
+			
+			for( i = 0; i < size; ++i ) ++stat[ _arr[ i ] ];
+		
+			tag = -1;
+		
+			for( i = 0; i < stat.Length; ++i )
+			{
+				if( stat[ i ] == 0 )
+				{
+					tag = i;
+					break;
+				}
+			}
+			
+			if( tag < 0 )
+			{
+				return -1;
+			}
+		
+			ptr = 0;
+			len = 1;
+			sym_prev = -1;
+		
+			_rle_arr[ ptr++ ] = ( ushort )tag;
+			
+			for( i = 0; i < size; ++i )
+			{
+				sym = _arr[ i ];
+		
+				if( sym_prev != sym || len >= ushort.MaxValue || i == size - 1 )
+				{
+					if( ptr >= _arr.Length )
+					{
+						return -1;
+					}
+					
+					if( len > 1 )
+					{
+						if( len == 2 )
+						{
+							_rle_arr[ ptr++ ] = ( ushort )sym_prev;
+						}
+						else
+						{
+							_rle_arr[ ptr++ ] = ( ushort )tag;
+							_rle_arr[ ptr++ ] = ( ushort )( len - 1 );
+						}
+					}
+		
+					_rle_arr[ ptr++ ] = ( ushort )sym;
+		
+					sym_prev = sym;
+		
+					len = 1;
+				}
+				else
+				{
+					++len;
+				}
+			}
+		
+			_rle_arr[ ptr++ ] = ( ushort )tag;	//end of file marked with zero length rle
+			_rle_arr[ ptr++ ] = 0;
+			
+			return ptr;
+		}
+	}
+}
---
+++ src/MAPeD/src/export/exporter_main.cs
@@ -0,0 +1,119 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: 0x8BitDev Copyright 2017-2023 ( MIT license. See LICENSE.txt )
+ * Date: 30.04.2023
+ * Time: 12:43
+ */
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace MAPeD
+{
+	/// <summary>
+	/// Description of exporter_main.
+	/// </summary>
+	public class exporter_main
+	{
+		private readonly Dictionary< int, exporter_data_chunk_base >	m_data_chunks_exporters;
+		
+		public const int	EXP_BLOCKS8		= 0x01;
+		public const int	EXP_BLOCKS16	= 0x02;
+		public const int	EXP_TILES		= 0x03;
+		public const int	EXP_PALETTE		= 0x04;
+		public const int	EXP_LAYOUT		= 0x05;
+		public const int	EXP_TILEMAP		= 0x06;
+		public const int	EXP_MAX			= 0x06; 
+		
+		private exporter_config m_config;
+		
+		public exporter_config config
+		{
+			set {}
+			get { return m_config; }
+		}
+		
+		private exporter_stats	m_stats;
+		
+		public exporter_main( data_sets_manager _data_mngr )
+		{
+			m_config = new exporter_config();
+			
+			m_data_chunks_exporters = new Dictionary< int, exporter_data_chunk_base >( EXP_MAX );
+			
+			// register( EXP_BLOCKS8, ... );
+			// register( EXP_BLOCKS16, ... );
+			// register( EXP_TILES, ... );
+			// register( EXP_PALETTE, ... );
+			// register( EXP_LAYOUT, ... );
+			// register( EXP_TILEMAP, ... );
+		}
+	}
+	
+	public class exporter_config
+	{
+		//...
+		
+		public exporter_config()
+		{
+			//...
+		}
+	}
+
+	public class exporter_stats
+	{
+		public int bin_data_length = 0;
+		
+		// compression data stats
+		//...
+		
+		public exporter_stats()
+		{
+			//...
+		}
+	}
+	
+	public abstract class exporter_data_chunk_base
+	{
+		protected readonly string				m_name;
+		
+		protected readonly exporter_stats		m_stats;
+		protected readonly exporter_config		m_config;
+		
+		protected readonly data_sets_manager	m_data_mngr;
+		
+		private i_compressor	m_compressor;
+
+		public i_compressor compressor
+		{
+			set { m_compressor = value; }
+		}
+		
+		private Func< ushort, ushort >	m_data_converter16;
+		
+		public Func< ushort, ushort > data_converter16
+		{
+			set { m_data_converter16 = value; }
+		}		
+		
+		public exporter_data_chunk_base( string _name, data_sets_manager _data_mngr, exporter_stats _stats, exporter_config _config )
+		{
+			m_name		= _name;
+			
+			m_stats		= _stats;
+			m_config	= _config;
+			
+			m_data_mngr	= _data_mngr;
+		}
+		
+		public abstract void export(	tiles_data		_data,
+										StreamWriter	_asmw,
+										BinaryWriter	_bw,
+										StreamWriter	_cw );
+		
+		public abstract void export(	layout_data		_data,
+										StreamWriter	_asmw,
+										BinaryWriter	_bw,
+										StreamWriter	_cw );
+	}
+}
